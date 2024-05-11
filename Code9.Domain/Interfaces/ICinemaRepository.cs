3105165e170bec707ab0bb577204a96dfb659730
ivvvv12 User <ivan.jelic.18@singimail.rs>
2024-05-11 01:15:10 -0700

    create update func

--- Code9.Domain/Interfaces/ICinemaRepository.cs
+++ Code9.Domain/Interfaces/ICinemaRepository.cs
@@ -5,5 +5,9 @@ namespace Code9.Domain.Interfaces
     public interface ICinemaRepository
     {
         public Task<List<Cinema>> GetAllCinemas();
+
+        public Task<Cinema> Add(Cinema Cinema);
+
+        public Task<Cinema> Update(Cinema Cinema);
     }
 }
