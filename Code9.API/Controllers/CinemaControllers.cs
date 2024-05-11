baac357b60adc7829c9b0018291360833e82df81
ivvvv12 User <ivan.jelic.18@singimail.rs>
2024-05-11 02:40:02 -0700

    task2

---
+++ Code9.API/Controllers/CinemaControllers.cs
@@ -0,0 +1,32 @@
+﻿using Code9.Domain.Models;
+using Code9.Domain.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.TagHelpers;
+
+namespace Code9.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CinemaControllers : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public CinemaControllers(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+     
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<Cinema>>> GetAllCinema()
+        {
+            var query = new GetAllCinemaQuery();
+            var cinema = await _mediator.Send(query);
+            return Ok(cinema);
+        }
+        
+        
+       
+    }
+}
